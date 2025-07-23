19581ef52532011469ad41c63e562bbd6dda2c1a
carllim-Max <carlimoficial034@hotmail.com>
2025-07-22 21:03:09 -0300

    Add .next/static/chunks/src_app_page_tsx_7f72f809._.js 1753228989032

---
+++ .next/static/chunks/src_app_page_tsx_7f72f809._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a7b38906._.js"
+],
+    source: "dynamic"
+});
